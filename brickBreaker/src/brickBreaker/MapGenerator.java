7adccc01902c687440118505279ac41355ae8f7c
TreySpin <treygilliland3@gmail.com>
2018-05-30 10:00:13 -0400

    Add color to bricks

--- brickBreaker/src/brickBreaker/MapGenerator.java
+++ brickBreaker/src/brickBreaker/MapGenerator.java
@@ -9,6 +9,10 @@ public class MapGenerator {
 	public int brickWidth;
 	public int brickHeight;
 	
+	public Color brickColor1;
+	public Color brickColor2;
+	public Color brickColor3;
+	
 	public MapGenerator(int row, int col) {
 		map = new int[row][col];
 		for(int i = 0; i < map.length; i++) {
@@ -17,14 +21,28 @@ public class MapGenerator {
 			}
 		}
 		brickWidth = 540/col;
-		brickHeight = 150/row;	
+		brickHeight = 150/row;
+		
+		/**
+		Color brickColor1 = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
+		Color brickColor2 = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
+		Color brickColor3 = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
+		**/
 	}
 	
 	public void draw(Graphics2D g) {
-		for(int i = 0; i < map.length; i++)
+		for(int i = 0; i < map.length; i++) {
+			Color currentColor;
+			if(i == 0)
+				currentColor = Color.BLUE;
+			else if(i == 1)
+				currentColor = Color.ORANGE;
+			else
+				currentColor = Color.GREEN;
+			
 			for(int j = 0; j < map[0].length; j++) {
 				if(map[i][j] > 0) {
-					g.setColor(Color.white);
+					g.setColor(currentColor);
 					g.fillRect(j * brickWidth + 80,  i * brickHeight + 50, brickWidth, brickHeight);
 					
 					g.setStroke(new BasicStroke(3));
@@ -32,7 +50,7 @@ public class MapGenerator {
 					g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 				}
 			}
-				
+		}
 	}
 	
 	public void setBrickValue(int value, int row, int col) {
